ab6dfe6d53fffd71eb9901b5f32ac6ebb21cc43f
AzadBatch21 <sadikhov.azad@gmail.com>
2020-12-02 12:45:33 -0600

    9th commit just  added comments

--- src/NewClass.java
+++ src/NewClass.java
@@ -9,5 +9,22 @@ public class NewClass {
         firstMethod.method1();
 
         System.out.println("new comments");
+
+        // create methodTwoBranch
+        // Modification
+        // Commit
+        // Check gitHub web
+        // Push
+        // called method2 from methodTwo class
+        /* Create a class "methodTwo"
+           Commit /Push/
+           // Call method3
+           Create a branch called methodTree
+           of the methodTwo
+           Create a class called methodThree
+           Commit/Push
+           Merge
+
+         */
     }
 }
